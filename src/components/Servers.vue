455cdd5490d8d868fc321720d113cd5fdeeddb05
WillyLu326 <luzh0326@gmail.com>
2017-10-03 22:29:52 -0400

    update

--- src/components/Servers.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="col-xs-12 col-sm-6">
-        <ul class="list-group">
-            <li class="list-group-item" v-for="server in 5">
-                Server #{{ server }}
-            </li>
-        </ul>
-    </div>
-</template>
