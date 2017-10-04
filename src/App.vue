ae8f42a42a68c7398a1a044f19bfe9ffd5f1f7fb
WillyLu326 <luzh0326@gmail.com>
2017-10-03 21:54:35 -0400

    implement App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="container">
-    
+    <app-user></app-user>
   </div>
 </template>
 
 <script>
-
+  import User from './components/User.vue';
+  export default {
+    components: {
+      'app-user': User
+    }
+  }
 </script>
 
 <style>
